171baba96e20f05bf6e856eea927ff7d6d03926b
Iago-RM <iagomahiques@gmail.com>
2025-09-17 17:03:35 -0300

    Add files via upload

--- MVC-Flask-main/MVC-flask/app.py
+++ MVC-Flask-main/MVC-flask/app.py
@@ -24,8 +24,8 @@ app.add_url_rule('/contact', view_func=UserController.contact, methods=['GET', '
 # Rotas Tarefas
 app.add_url_rule("/tasks", view_func=TaskController.list_tasks, endpoint="list_tasks")
 app.add_url_rule("/tasks/new", view_func=TaskController.create_task, methods=["GET", "POST"], endpoint="create_task")
-app.add_url_rule("/tasks/update/<int:task_id>", view_func=TaskController.update_task_status, methods=["POST"], endpoint="update_task_status")
-app.add_url_rule("/tasks/delete/<int:task_id>", view_func=TaskController.delete_task, methods=["POST"], endpoint="delete_task")
+app.add_url_rule("/tasks/update/<int:task_id>", view_func=TaskController.update_task_status, methods=["PUT"], endpoint="update_task_status")
+app.add_url_rule("/tasks/delete/<int:task_id>", view_func=TaskController.delete_task, methods=["DELETE"], endpoint="delete_task")
 
 
 if __name__ == '__main__':
